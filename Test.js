142c911d930d774964be2df01061e2c074fd05de
Sergey <li-on3i@mail.ru>
2019-04-08 19:23:50 +0400

    Home Work #3

--- Test.js
+++ Test.js
@@ -19,60 +19,73 @@ var appData = {
     budget: money,
     timeData: time,
     expenses: {},
-    income: '',
+    income: [],
     optionalExpenses:{} ,
-    savings: true
+    savings: true,
+    chooseExpenses: function(){
+        for(var i = 0; i < 2; i++){ 
+            let a = prompt("Введите обязательную статью расходов в этом месяце", '');
+            let b = +prompt("Во сколько обойдется?"); 
+           if(typeof(a) === "string" && a != '' && b != '' && typeof(b) != null && a.length < 50){
+               appData.expenses[a] = b;
+           } else {
+             i--;  
+           }
+       } 
+    },
+    detectDayBudget: function(){
+        appData.moneyPerDay = (appData.budget / 30).toFixed();
+        alert('Ежедневные выплаты: ' + appData.moneyPerDay);
+    },
+    detectLevel: function(){
+        if(appData.moneyPerDay <  100){
+            console.log("Минимальный уровень достатка");
+        }else if(appData.moneyPerDay >= 100 && appData.moneyPerDay < 2000){
+            console.log("Средний уровень достатка");
+        }else if(appData.moneyPerDay >= 2000){
+            console.log("Высокий уровень достатка");
+        }else{
+            console.log("Возникла ошибка в данных!");
+        } 
+    },
+    checkSavings: function(){
+        if(appData.savings == true){
+            let save = +prompt("Какова сумма накоплений?"),
+                percent = +prompt("Под какой процент?");
+    
+                appData.monthIncome = save/100/12*percent;
+                alert("Доход в месяц с ващего депозита: " + appData.monthIncome);
+        }  
+    },
+    chooseOptExpenses: function(){
+        for(let i = 1; i < 4; i++){
+            appData.optionalExpenses[i] = prompt("Статья необязательных расходов?");
+        }
+    },
+    chooseIncome: function(){
+        let items = prompt("Что принесёт дополнительный доход?");
+        while( typeof(items) != 'string' && items == '' && items == null){
+            items = prompt("Что принесёт дополнительный доход?");
+        }
+            appData.income = items.split(', ');
+            appData.income.push( prompt("Может что-то ещё?") );
+            appData.income.sort();
+
+            appData.income.forEach(function(item, i, arr){
+                i++;
+                alert("Способы доп. заработка: " + i + '-' + item);
+            })
+    }
 };
 
-
-function chooseExpenses(){
-    for(var i = 0; i < 2; i++){ 
-        let a = prompt("Введите обязательную статью расходов в этом месяце", '');
-        let b = +prompt("Во сколько обойдется?"); 
-       if(typeof(a) === "string" && a != '' && b != '' && typeof(b) != null && a.length < 50){
-           appData.expenses[a] = b;
-       } else {
-         i--;  
-       }
-   }
-}
-chooseExpenses();
-
-function detectDayBudget(){
-    appData.moneyPerDay = (appData.budget / 30).toFixed();
-    alert('Ежедневные выплаты: ' + appData.moneyPerDay);
-}
-    detectDayBudget();
-
-function detectLevel(){
-
-    if(appData.moneyPerDay <  100){
-        console.log("Минимальный уровень достатка");
-    }else if(appData.moneyPerDay >= 100 && appData.moneyPerDay < 2000){
-        console.log("Средний уровень достатка");
-    }else if(appData.moneyPerDay >= 2000){
-        console.log("Высокий уровень достатка");
-    }else{
-        console.log("Возникла ошибка в данных!");
-    }
-
-}
-detectLevel();
-
-function checkSavings(){
-    if(appData.savings == true){
-        let save = +prompt("Какова сумма накоплений?"),
-            percent = +prompt("Под какой процент?");
-
-            appData.monthIncome = save/100/12*percent;
-            alert("Доход в месяц с ващего депозита: " + appData.monthIncome);
-    }
+console.log("Наша программа включает в себя данные:");
+for( var key in appData){
+    console.log(key + ": " + appData[key]);
 }
 
-checkSavings();
 
-function chooseOptExpenses(){
-    for(let i = 1; i < 4){
-        appData.optionalExpenses.i = prompt("Статья необязательных расходов?");
-    }
-}
+
+
+
+
+
