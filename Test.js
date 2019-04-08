ac43b08b74e824044e80bb5e53915b0282d9737f
Sergey <li-on3i@mail.ru>
2019-04-08 12:19:50 +0400

    Home Work

--- Test.js
+++ Test.js
@@ -1,1 +1,18 @@
-"use strict"
+"use strict";
+
+var money = prompt('Ваш бюджет на месяц?');
+var time = prompt("Введите дату в формате YYYY-MM-DD");
+var appData = {};
+
+appData.money = money;
+appData.timeData = time;
+appData.expenses = {};
+appData.optionalExpenses = '';
+appData.income = '';
+appData.savings = false;
+
+appData.expenses.question1 = prompt("Введите обязательную статью расходов в этом месяце");
+appData.expenses.question2 = prompt("Во сколько обойдется?");  
+
+
+alert((+money - +appData.expenses.question2)/30);
